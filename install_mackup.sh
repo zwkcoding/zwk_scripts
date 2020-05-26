ab5236352990a9e485e62c0a1a4181ebb0416e97
zwk10367 <wenkai.zhou@uisee.com>
2020-05-26 11:55:37 +0800

    add install_vim8

--- install_mackup.sh
+++ install_mackup.sh
@@ -3,6 +3,7 @@
 set -e
 
 mkdir -p $HOME/dotfiles/
+cd $HOME/dotfiles
 git clone git@github.com:zwkcoding/mackup.git
 cd mackup
 cp .mackup.cfg ~/
---
+++ install_vim8.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+#sudo apt remove vim
+#sudo add-apt-repository --remove ppa:jonathonf/vim
